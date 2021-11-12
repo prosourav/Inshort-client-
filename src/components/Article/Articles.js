cb82f4f96d75cd40238bb90e92d0fe4123a01a9d
prosourav <sourav19961010@gmail.com>
2021-11-12 19:41:51 +0530

    final commit

--- src/components/Article/Articles.js
+++ src/components/Article/Articles.js
@@ -1,25 +1,33 @@
 import React, { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { getNews } from "../../services/api";
 import Article from "./Article.js";
 
 const Articles = () => {
   const [news, setNews] = useState([]);
+  const [page, setPage] = useState(0);
+
+  console.log("news", news);
 
   useEffect(() => {
     dailyNews();
-  }, []);
+  }, [page]);
 
   const dailyNews = async () => {
-    let response = await getNews();
-    setNews(response.data);
+    let response = await getNews(page);
+    setNews([...news, ...response.data]);
   };
 
   return (
-    <div>
+    <InfiniteScroll
+      dataLength={news.length} //This is important field to render the next data
+      next={() => setPage(page + 1)}
+      hasMore={true}
+    >
       {news.map((news) => {
         return <Article key={news._id} news={news} />;
       })}
-    </div>
+    </InfiniteScroll>
   );
 };
 
--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,14 @@ import axios from "axios";
 
 const url = "http://localhost:8000";
 
-export const getNews = async () => {
+export const getNews = async (page, size = 5) => {
+  console.log("api page", size);
+  console.log("api page", page);
   try {
-    return await axios.get(`${url}/news`);
+    return await axios.get(`${url}/news?page=${page}&size=${size}`);
   } catch (error) {
     console.log("Got eror while calling NewsApi: ", error);
   }
 };
+
+//
